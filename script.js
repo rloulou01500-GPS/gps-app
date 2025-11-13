a0c4fb5a7ef6159f00d6fef43bb53d74206dc432
rloulou01500-GPS <rloulou01500@gmail.com>
2025-11-13 20:04:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // URL brute GitHub du fichier radars.geojson
-const radarsURL = 'https://raw.githubusercontent.com/rloulou01500-GPS/gps-app/refs/heads/main/radars.geojson //?v=' + Date.now();
+const radarsURL = 'https://raw.githubusercontent.com/rloulou01500-GPS/gps-app/refs/heads/main/radars.geojson?v=' + Date.now();
 
 // Initialisation de la carte
 const map = L.map('map').setView([46.6, 2.5], 6); // Centre approximatif de la France
